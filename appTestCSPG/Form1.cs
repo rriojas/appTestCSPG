bfffb37d030f546d466292c86a0300f4a1c33bb7
Ruben Riojas <rriojas@gmail.com>
2024-03-11 19:49:17 -0600

    link

--- appTestCSPG/Form1.cs
+++ appTestCSPG/Form1.cs
@@ -6,6 +6,7 @@ namespace appTestCSPG
     public Form1()
     {
       InitializeComponent();
+      //https://www.kaggle.com/datasets/migeruj/videogames-predictive-model/data?select=dato.csv
       query = "SELECT * FROM datos;";
       dataGridView1.DataSource = Connection.SelectQuery(query);
 
